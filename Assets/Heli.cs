335480afbfa0263dce2c19336d89d3b6e0ccbb1c
Ben Kelly <benkellysoftware@gmail.com>
2020-03-18 09:17:01 +1100

    Tree obstacle hit

--- Assets/Heli.cs
+++ Assets/Heli.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Heli : MonoBehaviour
 {
     public float speed;
+    public int lives;
+    public Vector3 defaultPos;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
@@ -19,4 +21,16 @@ public class Movement : MonoBehaviour
         rb.AddForce(new Vector2( Input.GetAxis("Horizontal") * Time.deltaTime * speed, Input.GetAxis("Vertical") * Time.deltaTime * 300));
         
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Hit");
+        if (other.tag != "Obstacle") return;
+        {
+            lives--;
+            transform.position = defaultPos;
+            rb.velocity = Vector2.zero;
+        }
+    }
+    
 }
